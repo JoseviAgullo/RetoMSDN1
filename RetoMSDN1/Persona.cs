9086e0aaf984dc8f7da0fe5a5cfb0b6702a300ed
Josevi <josevi.agullo@gmail.com>
2014-09-12 13:24:37 +0200

    Override Equals and GetHash methods

--- RetoMSDN1/Persona.cs
+++ RetoMSDN1/Persona.cs
@@ -11,5 +11,25 @@ namespace RetoMSDN1
         public string Nombre { get; set; }
         public int Edad { get; set; }
 
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            Persona personaComparar = obj as Persona;
+            if (personaComparar == null)
+            {
+                return false;
+            }
+
+            return this.Nombre == personaComparar.Nombre && this.Edad == personaComparar.Edad;
+        }
+
+        public override int GetHashCode()
+        {
+            return Tuple.Create(this.Edad, this.Nombre).GetHashCode();
+        }
     }
 }
